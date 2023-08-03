eb750d8bd223ee764001c7fd4cf18f5fc9e510ea
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-08-03 21:34:01 +0200

    add mobile query

--- lethabo-portfolio/src/assets/App.jsx
+++ lethabo-portfolio/src/assets/App.jsx
@@ -22,13 +22,17 @@ function WorkAvailability() {
 //main script
 export default function App () {
 
+  
 
     return (
         <div className="main--container">
-            <nav className="nav-bar">
+            <nav>
                 <img src="./images/lethabo-logo-o-bg.png" width="90px" alt="Lethabo Logo" />
-                {/* <h4>Lethabo Mathabatha</h4> */}
-                {/*<h4 className="nav--bar-text">DEVELOPER, DESIGNER</h4> */}
+
+                {/* <div className="menu-icon" onClick={toggleMenu()}>
+                  <img src="/public/icon _Line Horizontal 3_.png" width={"40px"} alt='menu icon'></img> 
+                </div> */}
+               
                 <div className="nav--menu-items">
                     <a href="#about" style={{ textDecoration: "none" , color:"inherit"}} className="nav--menu-items">About </a>
                     -
@@ -57,7 +61,7 @@ export default function App () {
               <div className="about--section-text">
                   <span className="about--section-name">
                     <span className="about--section-name1">Lethabo</span>
-                    <span className="about--section-name2">Mathabatha</span>
+                    {/* <span className="about--section-name2">Mathabatha</span> */}
                     <img src="./images/arrow.png" alt="arrow" className="about--section-arrow"/>
                   </span>
 
@@ -162,26 +166,182 @@ export default function App () {
             </div>
 
             <div className="skills--section">
-              <span className="skills--section-header">Here is What I Use:</span>
+              <div className="skills--section-header-wrapper">
+                <span className="skills--section-header">Here is What I Use. </span>
+                <span className="skills--section-header">Here is What I Use. </span>
+                <span className="skills--section-header">Here is What I Use. </span>
+              </div>
               
               <div className="skills--section-container">
-                <img src="./icon _css3_.png" alt="CSS Icon" width={"40px"} /> 
-                <img src="./icon _html5_.png" alt="HTML Icon" width={"40px"} />
-                <img src="./icon _javascript_.png" alt="JavaScript Icon" width={"40px"} />
-                <img src="./icon _typescript_.png" alt="TypeScript Icon" width={"40px"} />
-                <img src="./icon _react_.png" alt="React Icon" width={"40px"} />
-                <img src="./icon _vue dot js_.png" alt="Vue Icon" width={"40px"} />
-                <img src="./icon _figma_.png" alt="Figma Icon" width={"30px"} />
-                <img src="./icon _github_.png" alt="Github Icon" width={"40px"} />
+                <div className="icon-container">
+                  <img src="./icon _css3_.png" alt="CSS Icon" width={"40px"} className="skills--section-icon"/>
+                  <span>CSS</span>
+                </div>
+
+                <div className="icon-container">
+                  <img src="./icon _html5_.png" alt="HTML Icon" width={"40px"} className="skills--section-icon"/>
+                  <span>HTML</span>
+                </div>
+
+                <div className="icon-container">
+                  <img src="./icon _javascript_.png" alt="JavaScript Icon" width={"40px"} className="skills--section-icon"/>
+                  <span>JavaScript</span>
+                </div>
+                
+                <div className="icon-container">
+                  <img src="./icon _typescript_.png" alt="TypeScript Icon" width={"40px"} className="skills--section-icon"/>
+                  <span>TypeScript</span>
+                </div>
+
+                <div className="icon-container">
+                  <img src="./icon _react_.png" alt="React Icon" width={"40px"} className="skills--section-icon"/>
+                  <span>React</span>
+                </div>
+
+                <div className="icon-container">
+                  <img src="./icon _vue dot js_.png" alt="Vue Icon" width={"40px"} className="skills--section-icon"/>
+                  <span>Vue</span>
+                </div>
+                
+                <div className="icon-container">
+                  <img src="./icon _figma_.png" alt="Figma Icon" width={"30px"} className="skills--section-icon"/>
+                  <span>Figma</span>
+                </div>
+
+                <div className="icon-container">
+                  <img src="./icon _github_.png" alt="Github Icon" width={"40px"} className="skills--section-icon"/>
+                  <span>Github</span>
+                </div>
               </div>
-              {/* <img src="/images/background-two.jpg" alt="Backgroung Image" className="skills--section-image" />
-              <div className="skills--section-overlay">
-                JavaScript
-              </div> */}
+              
+              <div className="contact--section">
+                <span className="contact--section-header">Contact</span>
+
+              
+                <div className="contact--section-buttons">
+                <Button 
+                  className="contact--section-contact"
+                  variant="contained"
+                    
+                  href="#contact"
+                  size="large"
+                  sx={{
+                    backgroundColor: "white",
+                    color: "black",
+                    textTransform: "none",
+                    size: "large",
+                    fontSize: "2.3rem",
+                    fontFamily: "Epilogue, sans-serif",
+                    fontWeight: "800",
+                    border: "1px solid black",
+                    borderRadius: "3rem",
+                    "&:hover": {
+                      backgroundColor: "black",
+                      color: "white",
+                    }
+                  }}
+                  onClick={() => {
+                      
+                  }}
+                >
+                email
+                </Button>
+
+                <Button 
+                  className="contact--section-github"
+                  variant="contained"
+                    
+                  href="#contact"
+                  size="large"
+                  sx={{
+                    backgroundColor: "white",
+                    color: "black",
+                    textTransform: "none",
+                    size: "large",
+                    fontSize: "2.3rem",
+                    fontFamily: "Epilogue, sans-serif",
+                    fontWeight: "800",
+                    border: "1px solid black",
+                    borderRadius: "3rem",
+                    "&:hover": {
+                      backgroundColor: "black",
+                      color: "white",
+                    }
+                  }}
+                >
+                github
+                </Button>
+
+                <Button 
+                  className="contact--section-linkedin"
+                  variant="contained"
+                    
+                  href="#contact"
+                  size="large"
+                  sx={{
+                    backgroundColor: "white",
+                    color: "black",
+                    textTransform: "none",
+                    size: "large",
+                    fontSize: "2.3rem",
+                    fontFamily: "Epilogue, sans-serif",
+                    fontWeight: "800",
+                    border: "1px solid black",
+                    borderRadius: "3rem",
+                    "&:hover": {
+                      backgroundColor: "black",
+                      color: "white",
+                    }
+                  }}
+                >
+                linkedin
+                </Button>
+                </div>
+
+                <div className="contact--section-logo-wrapper">
+                  <img src="./images/lethabo-logo-o-bg.png" alt="Lethabo Logo" className="contact--section-logo"/>
+
+                  <span className="contact--section-paragraph">As a new developer, I am always open to opportunities 
+                    to learn from and to share my skills where I can - get in touch!</span>
+
+                </div>
+              </div>
             </div>
 
+          {/* <div className="contact--section">
+            <img src="./images/lethabo-logo-o-bg.png" width="100px" alt="Lethabo Logo" />
+
+            
+
+
+
+                  <Button 
+                    className="contact--section-linkedin"
+                    variant="contained"
+                    
+                    href="#contact"
+                    size="large"
+                    sx={{
+                      backgroundColor: "white",
+                      color: "black",
+                      textTransform: "none",
+                      size: "large",
+                      fontSize: "2.3rem",
+                      fontFamily: "Epilogue, sans-serif",
+                      fontWeight: "800",
+                      border: "1px solid black",
+                      borderRadius: "3rem",
+                      "&:hover": {
+                        backgroundColor: "black",
+                        color: "white",
+                      }
+                    }}
+                  >
+                  linkedin
+                  </Button>
+          </div> */}
           <footer>
-            <p>designed & developed by lethabo mathabatha ~ 2023</p>
+            <p>designed & developed by lethabo mathabatha | 2023</p>
           </footer>
         </div>
         
