163aad8b044aa48013c188357984ac4902346952
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-08-06 13:49:10 +0200

    deleted unused css file

--- lethabo-portfolio/src/main.jsx
+++ lethabo-portfolio/src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './assets/stylesheet.css'
 import App from './assets/App'
-import { GroupOrientation } from './assets/App'
 
 
 
@@ -10,6 +9,5 @@ import { GroupOrientation } from './assets/App'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <GroupOrientation />
-  </React.StrictMode>,
+ </React.StrictMode>,
 )
